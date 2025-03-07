ce03b9d4d7df5db6cbf7c92fcccab32bfe6ef8d4
Jim Leirvik <jim@jimleirvik.no>
2025-03-07 21:04:36 +0100

    feat: moved from gitea

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const
   defineConfig = require('vite').defineConfig;
   resolve = require('path').resolve,
   viteSingleFile = require('vite-plugin-singlefile').viteSingleFile,
-  handlebars = require('vite-plugin-handlebars');
+  handlebars = require('vite-plugin-handlebars'),
+  helpers = require('handlebars-helpers')();;
  
 
 module.exports = (resume) => {
@@ -10,6 +11,7 @@ module.exports = (resume) => {
     plugins: [
       viteSingleFile(),
       handlebars({
+        helpers: helpers,
         partialDirectory: resolve(__dirname, 'src/partials'),
         context: {
           resume: resume,
